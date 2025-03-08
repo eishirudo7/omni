9e3f79eff9f04d25ef673f3d6650faf5d7483906
Yorozuya <ti13.0093@gmail.com>
2025-03-09 01:24:51 +0700

    kembalikan middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,26 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { withAuth } from "next-auth/middleware"
 
-export function middleware(request: NextRequest) {
-  // Bypass semua autentikasi
-  return NextResponse.next()
-}
+export default withAuth({
+  callbacks: {
+    authorized: ({ req, token }) => {
+      if (req.nextUrl.pathname === "/login") {
+        return true;
+      }
+      return !!token;
+    },
+  },
+})
+
 export const config = {
-  matcher: []  // Kosongkan matcher agar tidak ada path yang diproteksi
-}
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - login
+     * - api (semua API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+
+     */
+    '/((?!login|api|_next/static|_next/image|favicon.ico).*)'
+  ]
+} 
